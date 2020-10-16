ad6f604f159c4ba3f33fde6fa0b805a0889ffe3c
Daitong Xu <xudaitong@gmail.com>
2020-10-16 15:57:11 -0400

    Initial Commit

---
+++ FitHealth+/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FitHealth+
+//
+//  Created by xu daitong on 10/16/20.
+//  Copyright © 2020 xu daitong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
