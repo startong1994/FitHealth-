0c81bab5f52243c9aa948f11af17e3c2f6b846da
Daitong Xu <xudaitong@gmail.com>
2020-10-16 16:01:33 -0400

    created 3rd and 4th tabs,

---
+++ FitHealth+/ForthViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ForthViewController.swift
+//  FitHealth+
+//
+//  Created by xu daitong on 10/16/20.
+//  Copyright © 2020 xu daitong. All rights reserved.
+//
+
+import UIKit
+
+class ForthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
