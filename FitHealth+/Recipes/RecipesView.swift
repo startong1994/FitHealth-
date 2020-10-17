755eb3c85cf6a94ca446dc29a53cf9131ce8d83f
Daitong Xu <xudaitong@gmail.com>
2020-10-17 12:03:03 -0400

    template set up

--- FitHealth+/Recipes/RecipesView.swift
+++ FitHealth+/Recipes/RecipesView.swift
@@ -12,6 +12,7 @@ class RecipesView: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
