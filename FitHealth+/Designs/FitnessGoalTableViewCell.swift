debd7ae120452a2b7b3bd8f1906cdc6075a6f619
Daitong Xu <xudaitong@gmail.com>
2020-10-21 18:25:05 -0400

    designed workout goal cells

---
+++ FitHealth+/Designs/FitnessGoalTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  FitnessGoalTableViewCell.swift
+//  FitHealth+
+//
+//  Created by xu daitong on 10/21/20.
+//  Copyright © 2020 xu daitong. All rights reserved.
+//
+
+import UIKit
+
+class FitnessGoalTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var fitnessGoalTitle: UILabel!
+    @IBOutlet weak var fitnessCurrent: UILabel!
+    @IBOutlet weak var fitnessGoal: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        progressBar.transform = progressBar.transform.scaledBy(x: 1, y: 20)
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    @IBAction func editButtonPressed(_ sender: UIButton) {
+        print("presed")
+        
+    }
+    
+}
---
+++ FitHealth+/Fitness/FitnessGoalViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FitnessGoal.swift
+//  FitHealth+
+//
+//  Created by xu daitong on 10/21/20.
+//  Copyright © 2020 xu daitong. All rights reserved.
+//
+
+import UIKit
+
+class FitnessGoalViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.tableView.dataSource = self
+        
+        tableView.register(UINib(nibName: "FitnessGoalTableViewCell", bundle: nil), forCellReuseIdentifier: "fitnessGoalCell")
+    }
+
+
+}
+
+extension FitnessGoalViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "fitnessGoalCell", for: indexPath) as! FitnessGoalTableViewCell
+        tableView.rowHeight = 320
+        
+        return cell
+    }
+    
+    
+}
