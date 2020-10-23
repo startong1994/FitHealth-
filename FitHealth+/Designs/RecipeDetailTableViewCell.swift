ec503e9cc5f0b3dbad6af3c8bbb5ea890ed4e899
Daitong Xu <xudaitong@gmail.com>
2020-10-23 13:29:09 -0400

    redesigned recipePage,

---
+++ FitHealth+/Designs/RecipeDetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeDetailTableViewCell.swift
+//  FitHealth+
+//
+//  Created by xu daitong on 10/23/20.
+//  Copyright © 2020 xu daitong. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ FitHealth+/Recipes/RecipeDetailVeiwController.swift
@@ -0,0 +1,56 @@
+//
+//  RecipeDetailVeiwController.swift
+//  FitHealth+
+//
+//  Created by xu daitong on 10/23/20.
+//  Copyright © 2020 xu daitong. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    @IBOutlet weak var titleOfRecipe: UILabel!
+    @IBOutlet weak var imageOfRecipe: UIImageView!
+    @IBOutlet weak var ServingSizeOfRecipe: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        navigationItem.title = "Recipe"
+        self.tableView.dataSource = self
+        
+        imageOfRecipe.image = UIImage.init(named: "chicken parmesan")
+        
+        tableView.register(UINib(nibName: "RecipeDetailTableViewCell", bundle: nil), forCellReuseIdentifier: "recipeDetailCell")
+        
+        
+        
+        
+    }
+    
+
+
+}
+
+extension RecipeDetailViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "recipeDetailCell", for: indexPath) as! RecipeDetailTableViewCell
+        tableView.rowHeight = 700
+        
+        
+        
+        return cell
+        
+    }
+    
+    
+    
+}
