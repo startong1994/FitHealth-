c9328dda12a4d459acb6f247202a7e4f1c7416b6
Daitong Xu <xudaitong@gmail.com>
2020-10-23 10:42:01 -0400

    updated workoutGuide page layout

---
+++ FitHealth+/Fitness/WorkoutGuideViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WorkoutGuideViewController.swift
+//  FitHealth+
+//
+//  Created by xu daitong on 10/23/20.
+//  Copyright © 2020 xu daitong. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutGuideViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        self.tabBarController?.tabBar.isHidden = true
+        
+    }
+    
+
+
+}
