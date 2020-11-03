41b432a78e080710f6f23685dba4c0a6008f094a
Daitong Xu <xudaitong@gmail.com>
2020-11-03 18:00:16 -0500

    updated password rule,

--- FitHealthPlus/WelcomePage/SignUpViewContoller.swift
+++ FitHealthPlus/WelcomePage/SignUpViewContoller.swift
@@ -24,13 +24,18 @@ class SignUpViewContoller: UIViewController {
     
 
     @IBAction func registerPressed(_ sender: UIButton) {
+        let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        
         if let email = emailTextField.text, let password = passwordTextField.text {
             
             if email.hasSuffix("uncc.edu"){
 
             Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
                 if let e = error{
-                    print(e)
+                    let alert = UIAlertController(title: "Error", message: e.localizedDescription, preferredStyle: .alert)
+                    alert.addAction(ok)
+                    self.present(alert, animated: true, completion: nil)
                 }
                 else{
                     self.performSegue(withIdentifier: "registerToSignIn", sender: self)
@@ -41,7 +46,6 @@ class SignUpViewContoller: UIViewController {
             else{
                 
                 let alert = UIAlertController(title: "Error", message: "Please Enter UNCC School Email", preferredStyle: .alert)
-                let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
                 
                 alert.addAction(ok)
                 present(alert, animated: true, completion: nil)
